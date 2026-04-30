2632d03ae4dfb52725b399b7881765b6089479fe
Joscarlui <Joscarluij@gmail.com>
2026-04-30 09:47:47 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-const CACHE='gastos-v2';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/', '/manifest.json', '/icon-512.png'])));self.skipWaiting()});
+const CACHE='gastos-v3';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
+  self.skipWaiting();
+});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
