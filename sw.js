9acfa056569b770afc91ef5f7b56ca30671ab019
Joscarlui <Joscarluij@gmail.com>
2026-04-30 11:10:46 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='gastos-v3';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
